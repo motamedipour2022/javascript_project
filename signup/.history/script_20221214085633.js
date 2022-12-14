05adf0ca9f66828a4900fd1f0af93ea380bcd3c3
Mohsen Motamedipour <m.motamedipour2022@yahoo.com>
2022-12-14 09:44:07 +0330

    first_project

---
+++ signup/.history/script_20221214085633.js
@@ -0,0 +1,9 @@
+const container = document.
+querySelector(".container");
+
+const signUpBtn = document.
+querySelector(".green-bg button");
+
+signUpBtn.addEventListener("click", () => {
+  container.classList.toggle("change");
+});
